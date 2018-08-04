0ef79e5f4057afb7adc39175db83707f62ef8bee
nj-app <nilay.pal@outlook.com>
2018-08-04 16:42:15 -0400

    First commit

--- SurRealEstates/ViewController.swift
+++ SurRealEstates/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var businessDescription: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var backgroundImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: businessDescription.bottomAnchor).isActive = true
+
+        FillDescription()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func FillDescription(){
+        let tagLine: String = "aim for the stars, shoot for the moon!"
+        let desc: String = """
+Haven't you always dreamed about owning real estate outside Earth?
+
+At SurReal Estates, we have developed a path-breaking technology to make your dream come true. Using the latest state-of-the-art mapping techniques and long-distance laser engravings, we now give you the chance to own your own piece of land on the Moon.
+
+Come, visit us today and claim your (sur)real estate!
+"""
+        businessDescription.text = desc
+    }
 
 }
 
